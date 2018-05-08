ad075358108c7b1b2fe66f805ae0e8232ea80b21
dopexxx <jannis.born@gmx.de>
2018-05-08 13:02:23 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -439,10 +439,10 @@ if __name__ == '__main__':
             # TESTING 2
             # Set initial decoder input to be 0
 
-            write_dec_input = np.zeros((len(X_test)-1, 1)) + dict_char2num_y['<GO>']
+            write_dec_input = np.zeros((len(X_test), 1)) + dict_char2num_y['<GO>']
             # Generate character by character (for the entire batch, weirdly)
             for i in range(y_seq_length):
-                write_test_logits = sess.run(model_write.logits, feed_dict={model_write.keep_prob:1.0, model_write.inputs:X_test, model_write.outputs:write_dec_input})
+                write_test_logits = sess.run(model_write.logits, feed_dict={model_write.keep_prob:1.0, model_write.inputs:X_test[:,1:], model_write.outputs:write_dec_input})
                 write_prediction = write_test_logits[:,-1].argmax(axis=-1)
                 #print('Loop',test_logits.shape, test_logits[:,-1].shape, prediction.shape)
                 write_dec_input = np.hstack([write_dec_input, write_prediction[:,None]])
@@ -454,7 +454,7 @@ if __name__ == '__main__':
 
             # Test READING
             if args.reading:
-                read_dec_input = np.zeros((len(X_test)-1, 1)) + dict_char2num_x['<GO>']
+                read_dec_input = np.zeros((len(X_test), 1)) + dict_char2num_x['<GO>']
                 # Generate character by character (for the entire batch, weirdly)
                 for i in range(x_seq_length):
                     read_test_logits = sess.run(model_read.logits, feed_dict={model_read.keep_prob:1.0, model_read.inputs:Y_test[:,1:], model_read.outputs:read_dec_input})
