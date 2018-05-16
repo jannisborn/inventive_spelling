a9f8af61696f14334c3ce0de9b4a5dc585d81beb
dopexxx <jannis.born@gmx.de>
2018-05-16 22:00:26 +0200

    Changing some stuff

--- Code/bLSTM.py
+++ Code/bLSTM.py
@@ -159,10 +159,10 @@ class bLSTM(object):
 			# Loss function
 			if self.learn_type == 'normal':
 				self.loss = tf.contrib.seq2seq.sequence_loss(self.logits, self.targets, tf.ones([self.batch_size, self.output_seq_length]))
+				print(type(self.loss))
 			elif self.learn_type == 'lds':
 				self.loss, self.read_inps = tf.contrib.seq2seq.sequence_loss_lds(self.logits, self.targets, 
 					tf.ones([self.batch_size, self.output_seq_length]), self.alternative_targets)
-				self.loss = np.array(self.loss)
 			else:
 				raise ValueError("Unspecified learning regime.")
 
