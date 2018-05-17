b65bf06db247707c18fb49a4d962761a8a4cbb31
dopexxx <jannis.born@gmx.de>
2018-05-18 01:52:39 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -522,7 +522,7 @@ if __name__ == '__main__':
                 for batch_i, (write_inp_batch, write_out_batch, write_alt_targs) in enumerate(utils.batch_data(X_train, Y_train, args.batch_size, Y_alt_train)):
 
                     _, batch_loss, write_new_targs, batch_logits, rat = sess.run([model_write.optimizer, model_write.loss_lds, model_write.read_inps, model_write.logits,
-                        model_read.rat], 
+                        model_write.rat], 
                                     feed_dict = 
                                                             {model_write.keep_prob: args.dropout, model_write.inputs: write_inp_batch[:,1:], 
                                                             model_write.outputs: write_out_batch[:, :-1], model_write.targets: write_out_batch[:, 1:], 
