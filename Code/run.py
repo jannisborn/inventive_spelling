bdfc834839ec1fa32b002f07e0a9e4b7da9f2594
dopexxx <jannis.born@gmx.de>
2018-07-09 13:49:08 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -616,14 +616,13 @@ if __name__ == '__main__':
 
                 for k, (write_inp_batch, write_out_batch, write_alt_targs) in enumerate(utils.batch_data(X_train, Y_train, args.batch_size, Y_alt_train)):
 
-                    print("BEF")
-                    _, batch_loss, write_new_targs, w_batch_logits = sess.run([model_write.optimizer, model_write.loss_lds, 
-                        model_write.read_inps, model_write.logits], 
+                    _, batch_loss, write_new_targs, rat, batch_loss_reg, w_batch_logits = sess.run([model_write.optimizer, model_write.loss_lds, 
+                        model_write.read_inps, model_write.rat, model_write.loss_reg, model_write.logits], 
                                                                         feed_dict = {model_write.keep_prob:1.0, model_write.inputs: write_inp_batch[:,1:], 
                                                                             model_write.outputs: write_out_batch[:, :-1], model_write.targets: write_out_batch[:, 1:],
                                                                             model_write.alternative_targets: write_alt_targs[:,1:,:]})
-                    #print("LdS loss is " + round(crossent,2) + " while regular loss would be " + round(crossent_reg,2))
-                    #print("Ratio of words that were 'correct' in LdS sense: " + str(rat))
+                    print("LdS loss is " + round(batch_loss,2) + " while regular loss would be " + round(batch_loss_reg,2))
+                    print("Ratio of words that were 'correct' in LdS sense: " + str(rat))
 
 
                     write_epoch_loss += batch_loss
