1557eba1277f5c8a41bd9873e6639c8c4fe407d3
dopexxx <jannis.born@gmx.de>
2018-07-09 10:25:19 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -532,8 +532,10 @@ if __name__ == '__main__':
 
             elif regime == 'lds':
 
+
                 for batch_i, (write_inp_batch, write_out_batch, write_alt_targs) in enumerate(utils.batch_data(X_train, Y_train, args.batch_size, Y_alt_train)):
 
+                    print("BEF")
                     _, batch_loss, write_new_targs, batch_logits, rat = sess.run([model_write.optimizer, model_write.loss_lds, model_write.read_inps, model_write.logits,
                         model_write.rat], 
                                     feed_dict = 
@@ -541,6 +543,7 @@ if __name__ == '__main__':
                                                             model_write.outputs: write_out_batch[:, :-1], model_write.targets: write_out_batch[:, 1:], 
                                                             model_write.alternative_targets: write_alt_targs[:,1:,:]})
                     #print("Ratio of words that were 'correct' in LdS sense: " + str(rat))
+                    print("AF")
 
                 if args.reading:
                     read_inp_batch = write_new_targs
