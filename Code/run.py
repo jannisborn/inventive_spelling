10e8485064cde1e3a84f72910ae57be49e78c181
dopexxx <jannis.born@gmx.de>
2018-07-11 10:37:47 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
     indices = range(len(inputs))
     
     #if args.learn_type == 'normal':
-    X_train, X_test,Y_train, Y_test, indices_train, indices_test = train_test_split(inputs, targets, indices, test_size=args.test_size, random_state=args.seed)
+    #X_train, X_test,Y_train, Y_test, indices_train, indices_test = train_test_split(inputs, targets, indices, test_size=args.test_size, random_state=args.seed)
     #elif args.learn_type == 'lds':
     X_train, X_test,Y_train, Y_test, Y_alt_train_l, Y_alt_test_l, indices_train, indices_test = train_test_split(inputs, targets, alt_targets, indices, test_size=args.test_size, random_state=args.seed)
     max_len = max([len(l) for l in Y_alt_train_l])
