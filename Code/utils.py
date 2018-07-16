ef7d35fd809fe6fb510761d2ab67e94efc101176
dopexxx <jannis.born@gmx.de>
2018-07-17 00:31:02 +0200

    Changing some stuff

--- Code/utils.py
+++ Code/utils.py
@@ -747,7 +747,7 @@ def celex_retrieve(learn_type):
     phon_dict = np_dict_to_dict(data['phon_dict'])
     word_dict = np_dict_to_dict(data['word_dict'])
 
-    path = 'data/celex_alt_targets.npy'
+    path = '../../Models/data/celex_alt_targets.npy'
     print("Loading alternative targets ...")
     alt_targs_raw = np.load(path)
 
@@ -766,7 +766,7 @@ def childlex_retrieve():
     phon_dict = np_dict_to_dict(data['phon_dict'])
     word_dict = np_dict_to_dict(data['word_dict'])
 
-    path = '../../Models/data/childlex_alt_targets.npy'
+    path = 'data/childlex_alt_targets.npy'
     print("Loading alternative targets ...")
     alt_targs_raw = np.load(path)
 
