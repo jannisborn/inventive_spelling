34e76727e31c9ab8e89d6fb9a2d76a4803e7d73f
dopexxx <jannis.born@gmx.de>
2018-08-07 18:43:31 +0200

    Changing some stuff

--- Code/run.py
+++ Code/run.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
 
     elif args.task == 'childlex':
         ((inputs, targets) , (dict_char2num_x, dict_char2num_y), alt_targets) = utils.childlex_retrieve()
-        mas = 40000
+        mas = 100
 
     elif args.task == 'fibel':
         ((inputs, targets) , (dict_char2num_x, dict_char2num_y), alt_targets) = utils.fibel_retrieve()
