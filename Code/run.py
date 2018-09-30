8b3a6decf3f2135142a814aeb25df650f87620e1
dopexxx <jannis.born@gmx.de>
2018-09-30 19:37:28 +0200

    typo

--- Code/run.py
+++ Code/run.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
 
     if args.task == 'celex' :
         ((inputs, targets) , (dict_char2num_x, dict_char2num_y), alt_targets) = utils.celex_retrieve()
-        mas = 500
+        mas = 96
 
     elif args.task == 'celex_all':
         ((inputs, targets) , (dict_char2num_x, dict_char2num_y), alt_targets) = utils.celex_all_retrieve()
